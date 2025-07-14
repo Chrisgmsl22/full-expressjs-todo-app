f864d25b46a8520a02c3def10cbe64277edd639b
ChristianGM <chrisgmag22@gmail.com>
2025-07-14 12:12:36 -0600

    First commit of many

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Defining a Get method
+app.get("/", (_req, res) => {
+    res.send("Welcome to my Express API using TSs");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
