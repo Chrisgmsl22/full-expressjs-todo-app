fdb7a8477ab2d330a585db23ead2d66c7023b2d0
Christian Gonzalez <chrisgmag22@gmail.com>
2025-08-11 15:32:51 -0600

    added controller basic endpoints plus error handling

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,25 +1,98 @@
 import { NextFunction, Request, Response } from "express";
 import { IPostTask } from "../models/interfaces";
 import { generateStaticPosts } from "../utils";
+import mongoose from "mongoose";
+
+const STATIC_POSTS: IPostTask[] = generateStaticPosts();
 
 export class PostController {
     public static async getAllPosts(
-        req: Request,
+        _req: Request,
         res: Response,
         next: NextFunction
     ): Promise<void> {
-        const staticPosts: IPostTask[] = generateStaticPosts();
-
         try {
-            res.status(200).json(staticPosts);
+            res.status(200).json(STATIC_POSTS);
         } catch (err) {
             next(err);
         }
     }
 
-    public static async getPostById() {}
+    public static async getPostById(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const { id } = req.params;
+            const post = STATIC_POSTS.find((p) => p._id === id);
 
-    public static async createPost() {}
+            if (!post) {
+                res.status(404).json({ message: "Post was not found" });
+                return;
+            }
 
-    public static async deletePost() {}
+            res.status(200).json(post);
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    public static async createPost(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const { title, completed } = req.body;
+
+            // Validation to make sure these types are valid
+            if (typeof title !== "string" || !title.trim()) {
+                const errJsonMessage = {
+                    message: "Title is required and must be of type string",
+                };
+                res.status(400).json(errJsonMessage);
+                return;
+            }
+
+            if (completed !== undefined && typeof completed !== "boolean") {
+                const errMessage = {
+                    message: "Completed must be boolean",
+                };
+                res.status(400).json(errMessage);
+            }
+            const newPost: IPostTask = {
+                _id: new mongoose.Types.ObjectId().toString(),
+                title: title,
+                completed: completed || false,
+                createdAt: new Date(),
+            };
+
+            STATIC_POSTS.push(newPost);
+            res.status(201).json(newPost);
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    public static async deletePost(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const { id } = req.params;
+            const postIndex = STATIC_POSTS.findIndex((p) => p._id === id);
+
+            if (postIndex === -1) {
+                res.status(404).json({ message: "Post was not found" });
+                return;
+            }
+
+            STATIC_POSTS.splice(postIndex, 1);
+            res.status(204).send();
+        } catch (error) {
+            next(error);
+        }
+    }
 }
