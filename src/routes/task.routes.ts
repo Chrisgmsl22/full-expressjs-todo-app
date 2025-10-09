2b4c33b971fdbfce57bd114d29459c75f1658bff
Christian Gonzalez <chrisgmag22@gmail.com>
2025-10-09 09:19:26 -0600

    mounting auth layer and protecting routes

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { TaskController } from "../controllers/task.controller";
+import { authenticateJWT } from "../middleware";
 
 // We define a router
 const taskRoutes = Router();
@@ -9,17 +10,17 @@ taskRoutes.get("/", (_req, res) => {
     res.send("Welcome to my Express API using TS YAY2");
 });
 
-taskRoutes.get("/posts", TaskController.getAllTasks);
+taskRoutes.get("/posts",authenticateJWT, TaskController.getAllTasks);
 
 // Get a post by ID
-taskRoutes.get("/posts/:id", TaskController.getTaskById);
+taskRoutes.get("/posts/:id",authenticateJWT, TaskController.getTaskById);
 
 // Post a new Post
-taskRoutes.post("/posts", TaskController.createTask);
+taskRoutes.post("/posts",authenticateJWT, TaskController.createTask);
 
 // DELETE a post
-taskRoutes.delete("/posts/:id", TaskController.deleteTask);
+taskRoutes.delete("/posts/:id",authenticateJWT, TaskController.deleteTask);
 
-taskRoutes.put("/posts/:id", TaskController.updateTask);
+taskRoutes.put("/posts/:id",authenticateJWT, TaskController.updateTask);
 
 export default taskRoutes;
